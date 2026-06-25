77d8217461d61f04746f55d6820a3ffdd2dd786b
Gabriel <contato.joaogabriel@proton.me>
2026-06-25 01:11:53 -0300

    feat(website): capturing input

---
+++ source/code/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import { Steam } from "./entities/steam.js";
+
+(() => {
+  const _Steam = new Steam();
+  catchInputChange(_Steam);
+})();
+
+function catchInputChange(Steam) {
+  const SEARCH_INPUT = document.querySelector('input[name="search-steam-id"]');
+
+  SEARCH_INPUT.addEventListener("keydown", () => {
+    if (!(event.key === "Enter")) return false;
+    const isValidSteamId = Steam.validateSteamId(SEARCH_INPUT);
+
+    if (!isValidSteamId) SEARCH_INPUT.value = "Invalid Steam ID!!!";
+  });
+}
