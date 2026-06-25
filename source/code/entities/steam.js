b8735b72088675ee7c726a1027b6093144a11f3e
Gabriel <contato.joaogabriel@proton.me>
2026-06-25 01:28:03 -0300

    fix(typo): minor change URl -> URL

--- source/code/entities/steam.js
+++ source/code/entities/steam.js
@@ -7,14 +7,14 @@ export class Steam {
     }
 
     validateSteamId(SEARCH_INPUT) {
-        let IS_URl = false;
+        let IS_URL = false;
         let IS_SteamId = false;
 
         // TODO: Create a method to catch user ID via URL
-        if (SEARCH_INPUT.value.includes("steamcommunity.com")) IS_URl = true;
+        if (SEARCH_INPUT.value.includes("steamcommunity.com")) IS_URL = true;
         if (this.R_STEAM_ID.test(SEARCH_INPUT.value)) IS_SteamId = true;
 
-        if (!(IS_URl || IS_SteamId)) {
+        if (!(IS_URL || IS_SteamId)) {
             console.error(`${SEARCH_INPUT.value} is not a valid Steam ID`);
             return false;
         }
