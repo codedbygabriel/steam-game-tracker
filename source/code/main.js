7dfc97a0ff57ad08c1a5cbed9292111effa7041a
Gabriel <contato.joaogabriel@proton.me>
2026-06-25 03:38:05 -0300

    wip: searchSteamIdProfile

--- source/code/main.js
+++ source/code/main.js
@@ -3,17 +3,24 @@
 import { Steam } from "./entities/steam.js";
 
 (() => {
-  const _Steam = new Steam();
-  catchInputChange(_Steam);
+    const _Steam = new Steam();
+    catchInputChange(_Steam);
 })();
 
 function catchInputChange(Steam) {
-  const SEARCH_INPUT = document.querySelector('input[name="search-steam-id"]');
+    const SEARCH_INPUT = document.querySelector(
+        'input[name="search-steam-id"]',
+    );
 
-  SEARCH_INPUT.addEventListener("keydown", () => {
-    if (!(event.key === "Enter")) return false;
-    const isValidSteamId = Steam.validateSteamId(SEARCH_INPUT);
+    SEARCH_INPUT.addEventListener("keydown", () => {
+        if (!(event.key === "Enter")) return false;
+        const isValidSteamId = Steam.validateSteamId(SEARCH_INPUT);
 
-    if (!isValidSteamId) SEARCH_INPUT.value = "Invalid Steam ID!!!";
-  });
+        if (!isValidSteamId) {
+            SEARCH_INPUT.value = "Invalid Steam ID!!!";
+            return false;
+        }
+
+        Steam.searchSteamIdProfile();
+    });
 }
