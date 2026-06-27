f53aad3df7edbf75f38ef3ec1907c276c25ba7c5
Gabriel <contato.joaogabriel@proton.me>
2026-06-27 04:52:19 -0300

    refactor(code); variable names are now in camelCase

--- source/code/main.js
+++ source/code/main.js
@@ -3,31 +3,61 @@
 import { Steam } from "./entities/steam.js";
 
 (() => {
-    const _Steam = new Steam();
-    catchInputChange(_Steam);
+    const wrapper = document.querySelector(".search-wrapper");
+    const searchInput = document.querySelector('input[name="search-steam-id"]');
+    const button = document.querySelector(".search-steam-button");
+    const profiles = [];
+
+    init(searchInput, wrapper, button, profiles);
 })();
 
-function catchInputChange(Steam) {
-    const WRAPPER = document.querySelector(".search-wrapper");
-    const SEARCH_INPUT = document.querySelector(
-        'input[name="search-steam-id"]',
+function init(searchInput, wrapper, button, profiles) {
+    const steam = new Steam();
+
+    searchInput.addEventListener("focus", () =>
+        wrapper.classList.add("focused-input"),
     );
-
-    SEARCH_INPUT.addEventListener("focus", () =>
-        WRAPPER.classList.add("focused-input"),
+    searchInput.addEventListener("focusout", () =>
+        wrapper.classList.remove("focused-input"),
     );
-    SEARCH_INPUT.addEventListener("focusout", () =>
-        WRAPPER.classList.remove("focused-input"),
-    );
-
-    SEARCH_INPUT.addEventListener("keydown", async () => {
-        if (!(event.key === "Enter")) return false;
-        await Steam.validateSteamId(SEARCH_INPUT);
-        console.log(Steam.steamId);
-
-        if (!Steam.steamId) return false;
-
-        const games = await Steam.getSteamIdOwnedGames();
-        console.log(games);
+    button.addEventListener("click", async () => {
+        await inputEventHandler(steam, searchInput, profiles);
     });
+    searchInput.addEventListener("keydown", async () => {
+        await inputEventHandler(steam, searchInput, profiles);
+    });
+}
+
+const inputEventHandler = async (steam, searchInput, profiles) => {
+    const response = await steam.validateSteamId(searchInput);
+    if (!response) return false;
+
+    profiles.push(steam);
+    startGamesPanel(steam);
+};
+
+async function startGamesPanel(steam) {
+    const gamesPanel = document.querySelector(".games-panel");
+    const mainContentWrapper = document.querySelector(".main-content");
+    const panelItems = document.querySelector(".panel-items");
+    const gamesPanelTitle = document.querySelector(".games-panel-title");
+
+    mainContentWrapper.style.flexFlow = "row nowrap";
+    mainContentWrapper.style.alignItems = "start";
+    gamesPanelTitle.innerText = `Observing Games From (${steam.data.steamRealName || steam.data.steamName})`;
+
+    steam.data.games.forEach((game) => {
+        const gameWrapper = document.createElement("div");
+        const gameTitle = document.createElement("h2");
+        const gamePlayTime = document.createElement("small");
+
+        gameTitle.innerText = game.name;
+        gamePlayTime.innerText = game.playtime_forever;
+
+        gameWrapper.appendChild(gameTitle);
+        gameWrapper.appendChild(gamePlayTime);
+        panelItems.appendChild(gameWrapper);
+    });
+
+    gamesPanel.classList.remove("hidden");
 }
